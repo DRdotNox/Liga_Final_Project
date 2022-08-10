c9c62823d4b7c6a591c5bfa1253cfef4a9b16b15
Daria Roganova <droganova01@gmail.com>
2022-08-11 02:08:07 +0300

    add some fixes

--- src/main/java/com/liga/carwash/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/liga/carwash/filter/CustomAuthorizationFilter.java
@@ -1,19 +1,13 @@
 package com.liga.carwash.filter;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
+
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.liga.carwash.model.DTO.UserSecurityDTO;
-import com.liga.carwash.security.CustomUserPrincipal;
 import com.liga.carwash.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -36,29 +30,27 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if(request.getServletPath().equals("/login") || request.getServletPath().equals("v1/auth/refresh")){
-            filterChain.doFilter(request,response);
-        }
-        else {
+        if (request.getServletPath().equals("/login") || request.getServletPath().equals("v1/auth/refresh")) {
+            filterChain.doFilter(request, response);
+        } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
-            if(authorizationHeader!=null && authorizationHeader.startsWith("Bearer ")){
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 try {
                     String token = authorizationHeader.substring("Bearer ".length());
                     DecodedJWT decodedJWT = jwtService.validate(token);
                     String email = decodedJWT.getSubject();
-                    Collection <SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
                     Arrays.stream(decodedJWT.getClaim("role").asArray(String.class)).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
                     log.info(authorities.toString());
                     UsernamePasswordAuthenticationToken authenticationToken =
-                            new UsernamePasswordAuthenticationToken(email, null,authorities);
+                            new UsernamePasswordAuthenticationToken(email, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                    filterChain.doFilter(request,response);
+                    filterChain.doFilter(request, response);
 
                 } catch (Exception e) {
-                    jwtService.errorResponse(response,e);
+                    jwtService.errorResponse(response, e);
                 }
             } else {
-                //AnonymousAuthenticationToken token = new AnonymousAuthenticationToken("")
                 filterChain.doFilter(request, response);
             }
         }
